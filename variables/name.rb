31fcf2ccf9ce091571b1abe50d83eff29deb0bf6
Liz Seely <liz@alphawavept.com>
2020-05-13 20:23:22 -0400

    first two chapters done

---
+++ variables/name.rb
@@ -0,0 +1,8 @@
+puts "What is your first name?"
+  first_name = gets.chomp
+puts "What is your last name?"
+  last_name = gets.chomp
+puts "Welcome, #{first_name + " " + last_name}!"
+10.times do
+  puts first_name
+end
